04094b8343880cba11aae0ad2590af5100afe0ca
Paul McBride <hello@paulmcbride.net>
2022-01-04 13:46:50 +0000

    remove unused import

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import Image from "next/image";
 import styled from "styled-components";
 import styles from "../styles/Home.module.css";
 import YearGrid from "../components/YearGrid";
