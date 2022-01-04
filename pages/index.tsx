1c85282eb8ba382d8d8f4e9315016c9c0cdb82c4
Paul McBride <hello@paulmcbride.net>
2022-01-04 13:43:03 +0000

    add my name to footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -29,14 +29,11 @@ export default function Home() {
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://paulmcbride.net"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{" "}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
+          Built by Paul McBride
         </a>
       </footer>
     </div>
