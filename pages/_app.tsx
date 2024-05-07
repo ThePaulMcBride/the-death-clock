cad958e9a1e7f336e27ffecdf58641ab3b99ef89
Paul McBride <hello@paulmcbride.com>
2024-05-07 13:40:16 +0100

    remove errant semicolon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,9 @@
-import Script from "next/script";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </>
   );
 }
