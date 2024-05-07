9e3c49c6579dc46b0c42c27cd22ef4c457080595
Paul McBride <hello@paulmcbride.com>
2024-05-07 13:39:34 +0100

    switch to typescript

---
+++ pages/_app.tsx
@@ -0,0 +1,12 @@
+import Script from "next/script";
+import "../styles/globals.css";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Component {...pageProps} />;
+    </>
+  );
+}
+
+export default MyApp;
